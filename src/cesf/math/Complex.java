83a961ad5f7a1c7bfaecbe980c52cbde7ab28362
aitorUdabe <aitoruudabe@gmail.com>
2013-04-04 16:48:35 +0200

    Documentacion complex

--- src/cesf/math/Complex.java
+++ src/cesf/math/Complex.java
@@ -7,35 +7,29 @@ package cesf.math;
  * En un  nombre complex hi han diferents parts:
  * la part imaginaria y la part real.
  * La part imaginaria es b y la part real es a.
- * @author Aitor
+ * @author Aitor y Pau
  *
  */
-// Classe per disposar de nombres complexos
-// Els nombres complexos tenen la forma a+bi (ex. 3+4i),
-// on i és l'arrel quadrada de -1 i base dels nombres
-// imaginaris.
-// Les parts d'un nombre complex s'anomenen:
-// * a = part real
-// * b = part imaginària
-// Es poden calcular moltes coses amb nombres complexos
-// i accepten diverses operacions. Aquesta classe 
-// implementa les més destacables i necessàries.
 public class Complex {
-	private final double re;  // part real
-	private final double im;  // part imaginària
+	private final double re;  
 	
-	// Constructor per defecte
+	private final double im; 
+	
+	
+	/** constructor per defecte, que inicialitza 're' i 'im' a 0.*/
 	public Complex() { 
 		re = 0; 
 		im = 0; 
 	}
-	// Constructor amb paràmetres
+	/** Constructor que demana dos parametres del tipus double, 
+	 * i els asigna a les variables 're' i 'im' */
 	public Complex(double re, double im) {
 		this.re = re;
 		this.im = im;
 	}
 	
-    // Retorna una representació en cadena de text
+    /** en funcio dels valors de les variables esmentades abans, 
+     * retorna una representació en cadena de text */
     public String toString() {
         if (im == 0) return re + "";
         if (re == 0) return im + "i";
@@ -43,86 +37,118 @@ public class Complex {
         return re + " + " + im + "i";
     }
 
-    // Retorna el mòdul del complex
+    /** Realitza la operacio que retorna el mòdul del complex entre re i im.
+    @return  Complex de coordenades real,imag*/
     public double abs()   { return Math.hypot(re, im); }
-    // Retorna la fase (angle) del complex
+    /** Realitza la operacio que retorna la fase (angle) del complex entre re i im
+    @return  Complex de coordenades real,imag*/
     public double phase() { return Math.atan2(im, re); }
 
-    // Suma un segon complex i retorna el resultat
+    /**Funcio que realiza una suma entre dos complexos.Concretament afegeix 
+     * un segon complex al resultant de la funcio anterior.
+    @param  b
+    @return  Complex de coordenades real,imag*/
+    
     public Complex add(Complex b) {
         double real = this.re + b.re;
         double imag = this.im + b.im;
         return new Complex(real, imag);
     }
 
-    // Resta un segon complex i retorna el resultat
+    /**Funcio que realiza una resta entre dos complexos.Concretament resta 
+     * un segon complex al resultant de la funcio anterior.
+    @param  b
+    @return  Complex de coordenades real,imag*/    
     public Complex substract(Complex b) {
         double real = this.re - b.re;
         double imag = this.im - b.im;
         return new Complex(real, imag);
     }
 
-    // Multiplica per un segon complex i retorna el resultat
+    /**Funcio que realiza una multiplicacio entre dos complexos.Concretament multiplica 
+     * un segon complex al resultat de la funcio anterior.
+    @param  b
+   @return  Complex de coordenades real,imag*/
     public Complex multiply(Complex b) {
         double real = this.re * b.re - this.im * b.im;
         double imag = this.re * b.im + this.im * b.re;
         return new Complex(real, imag);
     }
 
-    // Multiplica per un real i retorna el resultat
+
+    /**Funcio que realiza una multiplicacio entre un nombre complex, i un de real.
+    @param  x
+    @return  Complex de coordenades real,imag*/
     public Complex multiply(double x) {
     	double real = x * this.re;
     	double imag = x * this.im;
         return new Complex(real, imag);
     }
 
-    // Retorna la divisió per un segon complex
+
+    /**Funcio que realiza una divisio entre dos complexos.Concretament divideix
+     * un segon complex al resultat de la funcio anterior.
+    @param  b*/
     public Complex divide(Complex b) {
         return this.multiply(b.reciprocal());
     }
 
-    // Retorna el complex conjugat de l'actual
+    /**Retorna el complex conjugat de l'actual
+     * @return  el complex resultat de la operacio*/
     public Complex conjugate() {
     	return new Complex(this.re, -this.im); 
     }
 
-    // Retorna el recíproc d'aquest complex
+    /** Retorna el recíproc d'aquest complex
+     * @return  el complex reciproc de la operacio*/
     public Complex reciprocal() {
         double x = this.re * this.re + this.im * this.im;
         return new Complex(this.re / x, -this.im / x);
     }
 
-    // getters
+    /** getters*/
     public double getReal() { return this.re; }
     public double getImag() { return this.im; }
 
-    // Retorna l'exponenciació del complex
+    /** Retorna l'exponenciació del complex
+     * @return  un nou complex resultant de la operacio*/
     public Complex exp() {
     	double real = Math.exp(this.re) * Math.cos(this.im);
     	double imag = Math.exp(this.re) * Math.sin(this.im);
         return new Complex(real, imag);
     }
 
-    // Retorna el sinus del complex
+    /** Retorna el sinus del complex
+     * @return  un nou complex resultant de la operacio de sinus*/
     public Complex sin() {
     	double real = Math.sin(re) * Math.cosh(im);
     	double imag = Math.cos(re) * Math.sinh(im);
         return new Complex(real, imag);
     }
 
-    // Retorna el cosinus del complex
+    /** Retorna el cosinus del complex
+     * 
+     *  * @return  un nou complex resultant de la operacio de cosinus*/
+     
     public Complex cos() {
     	double real = Math.cos(re) * Math.cosh(im);
     	double imag = -Math.sin(re) * Math.sinh(im);
         return new Complex(real, imag);
     }
 
-    // Retorna la tangent del complex
+    /** Retorna la tangent del complex
+     * 
+      * @return  un nou complex resultant de la operacio de tangent*/
     public Complex tan() {
         return sin().divide(cos());
     }
     
-    // versió estàtica de la funció suma
+    /** versió estàtica de la funció suma
+     * 
+     * @param a
+     * @param b
+     * @return  un nou complex resultant de la operacio*/
+    
     public static Complex add(Complex a, Complex b) {
         double real = a.re + b.re;
         double imag = a.im + b.im;
